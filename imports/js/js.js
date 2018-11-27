b9dad452673f20a7f40a6e8a486da02598c50c1b
unknown <cross50fire@gmail.com>
2018-11-27 09:31:32 -0200

    Ajustes #6

--- imports/js/js.js
+++ imports/js/js.js
@@ -1,4 +1,17 @@
 $(function(){
+	$.ajax({ 
+		method: "GET",
+		url: 'http://localhost:8080/export'
+	})
+	.done(function(result) {
+		console.log("success");
+	     json =  JSON.parse(result);
+	     console.log(json);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	
 	$("#welcome").removeClass("hidden");
 	$(".erroa").addClass("hidden");
 	$("#areae").addClass("hidden");
